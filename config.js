0ef2f2559339dc7485262f1ab624e836f70fd7fe
inferno254 <129995719+inferno254@users.noreply.github.com>
2024-12-05 16:03:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "EC1l1RKZ#iIk20jIp2r7-3tmLs0h2qfqTCnPfenAgsis_CVXc-3w",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n5vvij.jpg",
-SUDO_NB: process.env.SUDO_NB || "263780934873",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/qgvnvg.jpg",
+SUDO_NB: process.env.SUDO_NB || "254115061381",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
